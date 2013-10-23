6f6b37efa758db3368e649ac09d6f50843ea32ab
Santeri Paavolainen <santeri.paavolainen@codento.com>
2013-10-23 13:46:40 +0300

    Regeneration removes copyright headers. Works for me.

--- gen/fi/iki/santtu/android/locationtester/BuildConfig.java
+++ gen/fi/iki/santtu/android/locationtester/BuildConfig.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2013 Santeri Paavolainen <santtu@iki.fi>
- *
- * This file is part of LocationTester Android sample application.
- *
- * LocationTester is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LocationTester is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LocationTester.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 /*___Generated_by_IDEA___*/
 
 /** Automatically generated file. DO NOT MODIFY */
